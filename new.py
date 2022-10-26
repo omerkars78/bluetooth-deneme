09a949729ea8383902c63937da414d9e677c4ae4
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-10-26 11:30:56 +0300

    s
    s

---
+++ new.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from bleak import BleakScanner
+
+
+async def main():
+    print("scanning for 5 seconds, please wait...")
+
+    devices = await BleakScanner.discover(return_adv=True)
+    
+    for d, a in devices.values():
+        print()
+        print(d)                    # beacon names are POI
+        print("-" * len(str(d)))
+        print(a)
+    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- try.py
+++ try.py
@@ -44,13 +44,14 @@ async def main():
     print("scanning for 5 seconds, please wait...")
 
     devices = await BleakScanner.discover(return_adv=True)
-
+    
     for d, a in devices.values():
-        # print()
-        print(d)
-        # print("-" * len(str(d)))
-        # print(a)
-
+        print()
+        print(d)                    # beacon names are POI
+        print("-" * len(str(d)))
+        print(a)
+    
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
