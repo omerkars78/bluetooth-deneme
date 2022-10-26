25ab5007e395f1972f39d7559220fcbafb2a585c
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-10-26 14:27:42 +0300

    d
    d

--- newtry.py
+++ newtry.py
@@ -44,13 +44,12 @@ async def main():
     print("scanning for 5 seconds, please wait...")
 
     devices = await BleakScanner.discover(return_adv=True)
-
+    
     for d, a in devices.values():
         print()
         print(d)                    # beacon names are POI
         print("-" * len(str(d)))
         print(a)
-
     
     
 
