9855c35d28e1e3cdc70adc46b9b7f9de4b621051
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-11-18 15:49:02 +0300

    dd
    gfg

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 
 function App() {
   return (
-    <div className="flex flex-col bg-[#307BFE]">
-      <div></div>{/* Title */}
-      <div></div>{/* Circle */}
-      <div></div>{/* Select Option */}
+    <div className="flex flex-col bg-[#307BFE] w-screen h-screen">
+      <div>a</div>{/* Title */}
+      <div>a</div>{/* Circle */}
+      <div>a</div>{/* Select Option */}
     </div>
   );
 }
