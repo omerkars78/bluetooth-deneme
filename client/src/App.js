01d168a0d67c390a6e981695af234f5b58efdbf9
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-11-18 16:39:19 +0300

    dssddss
    dsdsdsdsds

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-
+import {AiOutlineArrowUp} from 'react-icons/ai'
 function App() {
   return (
-    <div className="flex flex-col bg-[#307BFE] w-screen h-screen">
-      <div>a</div>{/* Title */}
-      <div>a</div>{/* Circle */}
-      <div>a</div>{/* Select Option */}
+    <div className="flex flex-col bg-[#307BFE] w-full h-screen justify-between">
+          
+          <div className="text-center items-center text-white py-10 font-bold text-2xl">{/* Title */}
+            <h1 className="">Bina İçi Yönlendirme</h1>
+          </div>
+          
+          <div className="w-full bg-[#307BFE] flex p-4">
+          
+              <div className=" flex w-full bg-white rounded-15 text-black rounded-full items-center justify-center">
+                <div className='text-[350px]'>
+
+                <AiOutlineArrowUp /> 
+                </div>
+              </div>  
+          
+          </div>{/* Circle */}
+          
+          <div className='flex items-center justify-center text-3xl py-10 text-white font-bold'>
+            SELECT
+          </div>{/* Select Option */}
     </div>
   );
 }
