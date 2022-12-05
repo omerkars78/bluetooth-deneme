37e23b1eef115511b066105a5d9b0a573298425d
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-12-05 11:23:34 +0300

    s
    s

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 import asyncio
 from bleak import BleakScanner
 import asyncio
@@ -9,92 +9,15 @@ from construct.core import ConstError
 from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
-from data.data2 import get_devices as get_devices2
+from data2 import beacons
+
 app = Flask(__name__)
-# ibeacon_format = Struct(
-#     "type_length" / Const(b"\x02\x15"),
-#     "uuid" / Array(16, Byte),
-#     "major" / Int16ub,
-#     "minor" / Int16ub,
-#     "power" / Int8sl,
-# )
-# class UUIDEncoder(json.JSONEncoder):
-#     def default(self, uuid):
-#         if isinstance(uuid, UUID):
-#             # if the obj is uuid, we simply return the value of uuid
-#             return uuid.hex
-#         return json.JSONEncoder.default(self, uuid)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return jsonify(beacons)
 
-# @app.route("/beacons")
-# async def get_devices():
-#     devices = await BleakScanner.discover(return_adv=True)
-
-#     return devices.keys()
-
-
-@app.route("/beacons")
-def hello_world():
-    return get_devices2()
-
-# @app.route("/beacons")
-# def device_found():
-#     async def devices(
-#         device: BLEDevice, advertisement_data: AdvertisementData
-#     ):
-#         """Decode iBeacon."""
-#         try:
-#                 macadress = device.address
-#                 name = advertisement_data.local_name
-#                 apple_data = advertisement_data.manufacturer_data[0x004C]
-#                 ibeacon = ibeacon_format.parse(apple_data)
-#                 uuid = UUID(bytes=bytes(ibeacon.uuid))
-#                 minor = ibeacon.minor
-#                 major = ibeacon.major
-#                 power = ibeacon.power
-#                 rssi = device.rssi
-#                 rssi = int(rssi)
-
-#                 beacons = {
-#                     "Mac Adress": macadress,
-#                     "Local Name": name,
-#                     "UUID": uuid,
-#                     "Major": major,
-#                     "Minor": minor,
-#                     "TX Power": power,
-#                     "RSSI": rssi
-#                 }
-
-#                 if (beacons["Local Name"] == "POI" and beacons["RSSI"] <= -40 and beacons["RSSI"] >= -80):
-#                     return print(beacons)
-#                     # with open("data.json","a") as file:
-#                     #     json.dump(beacons,file,sort_keys=True,indent=4,skipkeys=True,cls=UUIDEncoder,separators=(",",":"))
-#                 else:
-#                     pass
-
-
-#         except KeyError:
-#             # Apple company ID (0x004c) not found
-#             pass
-#         except ConstError:
-#             # No iBeacon (type 0x02 and length 0x15)
-#             pass
-
-#     async def main():
-#         """Scan for devices."""
-#         scanner = BleakScanner()
-#         scanner.register_detection_callback(devices())
-
-        
-#         while (True):
-#             await scanner.start()
-#             await asyncio.sleep(0.5)
-#             await scanner.stop() 
-            
-#             asyncio.run(main())
 
 if __name__ == '__main__':
     app.run(host="193.140.27.106", port=5000, debug=True)
--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,42 +1,49 @@
-// First, let's get a list of all the devices that are visible
-
-navigator.bluetooth.requestDevice({
-    acceptAllDevices: true
-  }).then(function(device) {
-    // Now, let's get more information about each device
-    return device.gatt.connect();
-  })
-  .then(function(server) {
-    // Now we can get the detailed bluetooth data
-    return server.getPrimaryServices();
-  })
-//   .then(function(services) {
-//     // Now we can loop through each service and get its details
-//     for (let i = 0; i < services.length; i++) {
-//       let service = services[i];
-//       console.log('Service ' + i + ' UUID: ' + service.uuid);
-//       console.log('Service ' + i + ' Name: ' + service.name);
-//       // We can also get the characteristics of each service
-//       service.getCharacteristics().then(function(characteristics) {
-//         for (let j = 0; j < characteristics.length; j++) {
-//           let characteristic = characteristics[j];
-//           console.log(characteristic.uuid);)}
-
-
-
-// var bluetooth=  require('bluetooth');
-
-// bluetooth.on('discover', function(bluetoothDevice) {
-//     console.log('Found device: ' + bluetoothDevice.address);
-//     console.log('Name: ' + bluetoothDevice.name);
-//     console.log('Class: ' + bluetoothDevice.class);
-//     console.log('RSSI: ' + bluetoothDevice.rssi);
-//     console.log('Connectable: ' + bluetoothDevice.connectable);
-//     console.log('UUIDs: ' + bluetoothDevice.uuids);
-// 	console.log('Data: ' + bluetoothDevice.data);
+// // First, let's get a list of all the devices that are visible
+// document.getElementById("mybutton").addEventListener("click", function() {
+// navigator.bluetooth.requestDevice({
+//   acceptAllDevices: true
+// }).then(function(device) {
+//   // Now, let's get more information about each device
+//   return device.gatt.connect();
+// })
+// .then(function(server) {
+//   // Now we can get the detailed bluetooth data
+//   return server.getPrimaryServices();
+// })
+// .then(function(services) {
+//   // Now we can loop through each service and get its details
+//   for (let i = 0; i < services.length; i++) {
+//     let service = services[i];
+//     console.log('Service ' + i + ' UUID: ' + service.uuid);
+//     console.log('Service ' + i + ' Name: ' + service.name);
+//     // We can also get the characteristics of each service
+//     service.getCharacteristics()}})
+    
+//     .then(function(characteristics) {
+      
+//       for (let j = 0; j < characteristics.length; j++) {
+//         let characteristic = characteristics[j];
+//         console.log(characteristic.uuid);}
+//       }
+//     );
 // });
+  
 
-// bluetooth.startScanning();
+
+
+var bluetooth=  require('bluetooth');
+
+bluetooth.on('discover', function(bluetoothDevice) {
+    console.log('Found device: ' + bluetoothDevice.address);
+    console.log('Name: ' + bluetoothDevice.name);
+    console.log('Class: ' + bluetoothDevice.class);
+    console.log('RSSI: ' + bluetoothDevice.rssi);
+    console.log('Connectable: ' + bluetoothDevice.connectable);
+    console.log('UUIDs: ' + bluetoothDevice.uuids);
+	console.log('Data: ' + bluetoothDevice.data);
+});
+
+bluetooth.startScanning();
 
 // // function createAddItemWindow() {
 
